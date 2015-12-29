fda6d5c1e50eaacab067a1ed0586850566998b50
cquaid <codequaid@gmail.com>
2015-12-29 14:02:07 -0600

    Update for command list
    Add command.h
    Add command.c
    Add to TODO

---
+++ src/command.c
@@ -0,0 +1,189 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "command.h"
+#include "list.h"
+
+#define ARRAY_SIZ(x) (sizeof(x) / sizeof(*x))
+
+void
+command_list_clear(struct command_list *list)
+{
+	struct list_head *next;
+	struct list_head *entry;
+
+	list_for_each_safe(entry, next, &(list->head)) {
+		struct commadn *command;
+
+		list_del(entry);
+		command = command_entry(entry);
+		free(command);
+	}
+
+	command_list_init(list);
+}
+
+int
+register_command(struct command_list *list,
+	const char *name, command_fn_t handler,
+	const char *shortdoc, const char *longdoc)
+{
+	size_t slen;
+	struct command *command;
+
+	if (list == NULL || name == NULL)
+		return -EINVAL;
+
+	if (handler == (command_fn_t)0)
+		return -EINVAL;
+
+	if (name[0] == '\0')
+		return -EINVAL;
+
+	slen = strlen(name);
+
+	command = malloc(sizeof(*command) + slen);
+
+	if (command == NULL)
+		return -errno;
+
+	memcpy(command->name, name, slen + 1);
+
+	command->handler  = handler;
+	command->shortdoc = shortdoc;
+	comamnd->longdoc  = longdoc;
+
+	command->id = list->next_id++;
+
+	list_add(&(list->head), &(command->node));
+	list->size++;
+
+	return 0;
+}
+
+static struct command *
+find_command(struct command_list *list, const char *name)
+{
+	struct list_head *entry;
+
+	list_for_each(entry, &(list->head)) {
+		struct command *command = command_entry(entry);
+
+		if (strcmp(command->name, name) == 0)
+			return command;
+	}
+
+	return NULL;
+}
+
+int
+exec_line(struct command_list *list, const char *line)
+{
+	int err;
+
+	char *p;
+	char *pline;
+
+	size_t argc = 0;
+	char **argv = NULL;
+
+	size_t stack_pos = 0;
+	char *argv_stack[16];
+
+	struct command *command;
+
+	/* Empty string. */
+	if (line[0] == '\0')
+		return 0;
+
+	pline = strdup(line);
+
+	if (pline == NULL)
+		return -errno;
+
+	p = pline;
+
+	while (*p && isspace(*p)) ++p;
+
+	/* Just whitespace. */
+	if (*p == '\0')
+		return 0;
+
+	argv_stack[stack_pos++] = p;
+
+	for (;*p != '\0'; ++p) {
+		if (isspace(*p)) {
+			void *new_argv;
+
+			*p = '\0';
+			++p;
+
+			while (*p && isspace(*p)) ++p;
+
+			if (*p == '\0')
+				break;
+
+			/* Add next argument. */
+			if (stack_pos < ARRAY_SIZ(argv_stack)) {
+				argv_stack[stack_pos++] = p;
+				continue;
+			}
+
+			/* Filled up the stack, expand argv. */
+			new_argv = realloc(argv,
+				(argc + ARRAY_SIZ(argv_stack)) * sizeof(*argv));
+
+			if (new_argv == NULL) {
+				err = -errno;
+				goto out;
+			}
+
+			argv = new_argv;
+
+			memcpy(&argv[argc], argv_stack,
+				ARRAY_SIZ(argv_stack) * sizeof(*argv));
+
+			argc += ARRAY_SIZ(argv_stack);
+			stack_pos = 0;
+		}
+	}
+
+	if (argc == 0) {
+		argv = argv_stack;
+		argc = stack_pos;
+	}
+	else if (stack_pos != 0) {
+		void *new_argv;
+
+		/* We have both dynamicly allocated members and stack members. */
+		new_argv = realloc(argv, (argc + stack_pos) * sizeof(*argv));
+
+		if (new_argv == NULL) {
+			err = -errno;
+			goto out;
+		}
+
+		argv = new_argv;
+
+		memcpy(&argv[argc], argv_stack, stack_pos * sizeof(*argv));
+
+		argc += stack_pos;
+	}
+
+	command = find_command(list, argv[0]);
+
+	err = command->handler(argc, argv);
+
+out:
+
+	if (argv != NULL && argv != argv_stack)
+		free(argv);
+
+	free(pline);
+
+	return err;
+}
+
+#endif /* H_COMMAND */
---
+++ src/command.h
@@ -0,0 +1,56 @@
+#ifndef H_COMMAND
+#define H_COMMAND
+
+#include <stddef.h>
+
+#include "list.h"
+
+/* TODO: Hash Table */
+
+typedef int(*command_fn_t)(size_t, char **);
+
+struct command {
+	struct list_head node;
+
+	size_t id;
+
+	command_fn_t handler;
+
+	/* TODO: docs... */
+	const char *shortdoc;
+	const char *longdoc;
+
+	char name[1];
+};
+
+struct command_list {
+	struct list_head head;
+	size_t next_id;
+	size_t size;
+};
+
+
+static inline void
+command_list_init(struct command_list *list)
+{
+	list_head_init(&(list->head));
+	list->next_id = 1;
+	list->size = 0;
+}
+
+extern void command_list_clear(struct command_list *list);
+
+#define command_list_is_empty(command_list) \
+	list_is_empty(&((command_list)->head))
+
+#define command_entry(list_node) \
+	list_entry(list_node, struct command, node)
+
+
+extern int register_command(struct command_list *list,
+	const char *name, command_fn_t handler,
+	const char *shortdoc, const char *longdoc);
+
+extern int exec_line(struct command_list *list, const char *line);
+
+#endif /* H_COMMAND */
